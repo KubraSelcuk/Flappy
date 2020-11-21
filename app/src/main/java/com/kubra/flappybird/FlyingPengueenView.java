c1486d9296d55c95ec4b2eea57a36ffbfca56788
KubraSelcuk <rabiasel1999@gmail.com>
2020-11-22 01:23:39 +0300

    .

--- app/src/main/java/com/kubra/flappybird/FlyingPengueenView.java
+++ app/src/main/java/com/kubra/flappybird/FlyingPengueenView.java
@@ -7,51 +7,69 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class FlyingPengueenView extends View {
 
-    private Bitmap pengueen;
+    private int canvasWidth;
+    private int canvasHeight;
 
-    private Bitmap backgroundImage;
-    private Paint scorePaint = new Paint();
-    private Bitmap life[]=new Bitmap[2];
-
-    private Bitmap pengueen1[] = new Bitmap[2];
+    //private Bitmap pengueen;
+    private Bitmap pengueen[] = new Bitmap[2];
     private int pengueenX=10;
     private int pengueenY;
     private int pengueenSpeed;
 
-    private boolean touch = false;
+    private Bitmap backgroundImage;
 
-    private int canvasWidth, canvasHeight;
+    private Paint scorePaint = new Paint();
+
+    //private Paint levelPaint= new Paint();
+
+    private Bitmap life[]=new Bitmap[2];
+
+    private boolean touch= false;
+
 
     public FlyingPengueenView(Context context){
         super(context);
 
-        pengueen= BitmapFactory.decodeResource(getResources(), R.drawable.pengueen1);
+        pengueen[0]= BitmapFactory.decodeResource(getResources(), R.drawable.pengueen1);
+       pengueen[1]= BitmapFactory.decodeResource(getResources(), R.drawable.pengueen2);
 
         backgroundImage=BitmapFactory.decodeResource(getResources(), R.drawable.background);
 
         scorePaint.setColor(Color.BLACK);
-        scorePaint.setTextSize(70);
+        scorePaint.setTextSize(70); //70
         scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
         scorePaint.setAntiAlias(true);
+
+        //levelPaint.setColor(Color.DKGRAY);
+        //levelPaint.setTextSize(32);
+//  scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
+//  levelPaint.setTextAlign(Paint.Align.CENTER);
+//        scorePaint.setAntiAlias(true);
+
 
         life[0]= BitmapFactory.decodeResource(getResources(),R.drawable.hearts);
         life[1]= BitmapFactory.decodeResource(getResources(),R.drawable.heart_grey);
 
+
+        pengueenY=500;
     }
 
     @Override
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
+
         canvasWidth = canvas.getWidth();
         canvasHeight = canvas.getHeight();
 
 
-        int minPengueenY = pengueen1[0].getHeight();
-        int maxPengueenY =canvasHeight - pengueen1[0].getHeight() * 3;
+        int minPengueenY = pengueen[0].getHeight();
+        int maxPengueenY =canvasHeight - pengueen[0].getHeight() * 3;
+
         pengueenY = pengueenY + pengueenSpeed;
         if(pengueenY<minPengueenY)
         {
@@ -62,25 +80,47 @@ public class FlyingPengueenView extends View {
             pengueenY = maxPengueenY;
         }
         pengueenSpeed = pengueenSpeed +2;
+
         if(touch)
         {
 
-            canvas.drawBitmap(pengueen1[1],pengueenX,pengueenY,null);
+            canvas.drawBitmap(pengueen[1],pengueenX,pengueenY,null);
             touch=false;
         }
         else{
-            canvas.drawBitmap(pengueen1[1],pengueenX,pengueenY,null);
+            canvas.drawBitmap(pengueen[1],pengueenX,pengueenY,null);
         }
 
 
 
         canvas.drawBitmap(backgroundImage,0,0,null);
+
         canvas.drawText("Score:",20,60,scorePaint);
-        canvas.drawBitmap(pengueen,0,0,null);
+
+        //canvas.drawText("Level 1", canvasWidth()/2,60,levelPaint);
+
+        canvas.drawBitmap(pengueen[0],0,0,null);
+
+
+
         canvas.drawBitmap(life[0],580,10,null);
+
         canvas.drawBitmap(life[0],680,10,null);
+
         canvas.drawBitmap(life[0],780,10,null);
 
+
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction()==MotionEvent.ACTION_DOWN)
+        {
+            touch=true;
+            pengueenSpeed=-20;
+
+
+        }
+        return true;
 
     }
 }
--- app/src/main/java/com/kubra/flappybird/MainActivity.java
+++ app/src/main/java/com/kubra/flappybird/MainActivity.java
@@ -3,15 +3,22 @@ package com.kubra.flappybird;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity
+{
 
     private FlyingPengueenView gameView;
-
+    private Handler handler= new Handler();
+    private final static long TIMER_INTERVAL = 30;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -19,6 +26,23 @@ public class MainActivity extends AppCompatActivity {
         gameView = new FlyingPengueenView(this);
         setContentView(gameView);
 
+        Timer timer= new Timer();
+        timer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                handler.post(new Runnable()
+                {
+                                 @Override
+                                 public void run()
+                                 {
+                                     gameView.invalidate();
+                                 }
+                });
+
+            }
+        },0,TIMER_INTERVAL);
     }
 }
 
